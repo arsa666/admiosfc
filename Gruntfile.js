b8496ea9deb0a147650a84d68fc00cb6aaeb19d2
root <arsa666.k@gmail.com>
2015-03-29 15:37:24 -0400

    second commit with all players and opponents

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,34 +1,55 @@
 'use strict';
 
+//require('load-grunt-tasks')(grunt);
+
 module.exports = function(grunt) {
- 
+
   // Project configuration.
-    grunt.initConfig({
-    // This line makes your node configurations available for use
-	pkg: grunt.file.readJSON('package.json'),
-    // This is where we configure JSHint
-	jshint: {
-	    options: {
-                jshintrc: '.jshintrc',
-                browser: true,
-                reporter: require('jshint-stylish')
+	grunt.initConfig({
+		// This line makes your node configurations available for use
+		pkg: grunt.file.readJSON('package.json'),
+		jshint: {
+			options: {
+					jshintrc: '.jshintrc',
+					browser: true,
+					reporter: require('jshint-stylish')
+				},
+				all: [
+					'Gruntfile.js',
+					'js/**/*.js',
+				]
+		},
+		watch: {
+			js: {
+				files: [
+					'js/**/*.js',
+				],
+				tasks: ['jshint']
+			},
+			css: {
+                files: 'styles/*.scss',
+                tasks: ['sass'],
+                options: {
+                  livereload: true,
+                },
             },
-            all: [
-                'Gruntfile.js',
-                'js/**/*.js',
-            ]
-	},
-	watch: {
-            js: {
-                files: [
-                    'js/**/*.js',
-		],
-                tasks: ['jshint']
-            }
-	}
-    });
-    // Each plugin must be loaded following this pattern
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.registerTask('default', ['jshint', 'watch']);
+		},
+		sass: {
+				options: {
+	            // sourceMap: true
+	        },
+	        dist: {
+	            files: {
+	                'styles/main.css': 'styles/main.scss'
+	            }
+	        }
+
+	    }
+	});
+	//Each plugin must be loaded following this pattern
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	//grunt.registerTask('sass', 'sass');
+	grunt.registerTask('default', ['sass', 'jshint', 'watch']);
 };
--- js/functions.js
+++ js/functions.js
@@ -1,10 +1,34 @@
 'use strict';
 
-function tempFunc () {
+function makeDragabble () {
+    var players = $('.player');
 
-
-
+    _.each(players, function (player) {
+        $('#' + player.id).draggable();
+    });
 }
 
+$( document ).ready(function() {
+    console.log( 'ready!' );
 
-tempFunc();
+    var height = $(document).height();
+    var width = $(document).width();
+
+    $('.background').css('height', height + 'px');
+    $('.background').css('width', width + 'px');
+
+    makeDragabble();
+
+    $('#createOpponent').click(function (e) {
+        var opponentElem  = $('<div class="player"><label>Opponent</label><img src="images/opponent.png"  /></div>');
+
+        $('.background').append(opponentElem);
+
+        opponentElem.draggable();
+    });
+
+});
+
+
+
+
